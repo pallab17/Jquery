4a5e99b13e9b51890fe1786c4ef80bf725bd8da3
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-01-31 13:08:44 +0530

    Add files via upload

---
+++ Jquery/index.js
@@ -0,0 +1,21 @@
+// alert("workinh");
+
+// jodi amra jquery aar js r cdn link ta head section e include kori(css r link  r porer line e )
+// tahole high chances ache je query load howar somoye js access niye nite chailo
+// tate amra aar change ei korte parbona the web app using jquery 
+// soln ta ki then ?
+// $(document).ready(function(){
+//     $("h1").css("color", "red"); 
+// }
+// ) // etate ki hocche---> amra check korchi jokhun ei jquery libary ta add hocche tokhuni ei amra 
+// change korchi h1 tag r color ta 
+//jate js document take age access korar chestaye 
+// jquery take load ei korte dilo na
+// but amra erom $(document).ready function use korbo na
+// alternate soln hocche amra script link cdn gulo add korbo html file e
+// before  sesh r body tag jate we can ignore writing jquery ready fn
+
+
+
+
+ $("h1").css("color", "red");
