261930006466bb5fc3e41edce18685867be4b2e4
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-01-31 14:10:19 +0530

    Add files via upload

--- Jquery/index.js
+++ Jquery/index.js
@@ -2,20 +2,26 @@
 
 // jodi amra jquery aar js r cdn link ta head section e include kori(css r link  r porer line e )
 // tahole high chances ache je query load howar somoye js access niye nite chailo
-// tate amra aar change ei korte parbona the web app using jquery 
+// tate amra aar change ei korte parbona the web app using jquery
 // soln ta ki then ?
 // $(document).ready(function(){
-//     $("h1").css("color", "red"); 
+//     $("h1").css("color", "red");
 // }
-// ) // etate ki hocche---> amra check korchi jokhun ei jquery libary ta add hocche tokhuni ei amra 
-// change korchi h1 tag r color ta 
-//jate js document take age access korar chestaye 
+// ) // etate ki hocche---> amra check korchi jokhun ei jquery libary ta add hocche tokhuni ei amra
+// change korchi h1 tag r color ta
+//jate js document take age access korar chestaye
 // jquery take load ei korte dilo na
 // but amra erom $(document).ready function use korbo na
 // alternate soln hocche amra script link cdn gulo add korbo html file e
 // before  sesh r body tag jate we can ignore writing jquery ready fn
 
+$("h1").css("color", "red");
 
-
-
- $("h1").css("color", "red");
+// selecting elements with jquery
+//$("selector");
+//  document.querySelector("h1");   // select korchi ekta h1 element using query selector
+//  document.querySelectorAll("h1");   // select korchi sob kota h1 element using query selector all
+//  $("h1");  // select korchi h1 element using jquery
+// jquery ke short e dollar sign diye lekha jaye $ 
+// jokhun amra jquery diye element select korchi tokhun sob kota element ei target hocche with that selector naam
+// mane  $("h1"); ei code ta sob kota element with h1 tag ke target korche 
